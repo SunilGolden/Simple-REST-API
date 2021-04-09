e43e050ac9958b93c7b16da50b0c74a5d0ed9896
sunilgolden <gautam.sunil7725@gmail.com>
2021-04-09 17:08:56 +0545

    Add courses model

---
+++ models/courses.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+
+function validateCourses(data) {
+    const schema = Joi.object({
+        semster: Joi.string().allow(''),
+		subject: Joi.string().required(),
+		requiresLab: Joi.boolean().required(),
+		credits: Joi.number().min(0).required()
+    });
+
+    return schema.validate(data);
+}
+
+
+exports.validateCourses = validateCourses;
